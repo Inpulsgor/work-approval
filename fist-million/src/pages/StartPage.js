192969edf197ea3f8f52fbdeb260a19f8fd06640
Dmytro Smakovskyi <dmsmakovskyi@gmail.com>
2020-10-03 14:11:39 +0300

    start page added

--- fist-million/src/pages/StartPage.js
+++ fist-million/src/pages/StartPage.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+
+import { ReactComponent as Logo } from '../assets/icon.svg';
 
 const StartPage = () => {
   return (
-    <div className="start">
-      <h2>Start Page</h2>
-    </div>
+    <section className="start">
+      <div className="container">
+      <Logo className="logo"/>
+      <h2 className="title">Who wants to be a millionaire?</h2>
+      <Link className="button" to="/game">Start</Link>
+      </div>
+    </section>
   );
 };
 
